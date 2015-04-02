72895594f30eb581e78881275024925946821c6d
Peter Jihoon Kim <raingrove@gmail.com>
2015-04-02 16:23:25 -0700

    Selection Marshal/Unmarshal

--- ot/selection/selection.go
+++ ot/selection/selection.go
@@ -1,6 +1,14 @@
 package selection
 
-import "github.com/nitrous-io/ot.go/ot/operation"
+import (
+	"errors"
+
+	"github.com/nitrous-io/ot.go/ot/operation"
+)
+
+var (
+	ErrUnmarshalFailed = errors.New("ot/selection: unmarshal failed")
+)
 
 type Selection struct {
 	Ranges []Range `json:"ranges"`
@@ -12,4 +20,61 @@ func (s *Selection) Transform(op *operation.Operation) *Selection {
 		tr[i] = *r.Transform(op)
 	}
 	return &Selection{tr}
+}
+
+func (s *Selection) Marshal() map[string]interface{} {
+	mr := make([]map[string]interface{}, len(s.Ranges))
+	for i, r := range s.Ranges {
+		mr[i] = map[string]interface{}{"anchor": r.Anchor, "head": r.Head}
+	}
+	return map[string]interface{}{"ranges": mr}
+}
+
+func Unmarshal(data map[string]interface{}) (*Selection, error) {
+	if data["ranges"] == nil {
+		return nil, ErrUnmarshalFailed
+	}
+
+	dr, ok := data["ranges"].([]interface{})
+	if !ok {
+		return nil, ErrUnmarshalFailed
+	}
+
+	ranges := make([]Range, len(dr))
+
+	for i, o := range dr {
+		r, ok := o.(map[string]interface{})
+		if !ok {
+			return nil, ErrUnmarshalFailed
+		}
+		rng, err := unmarshalRange(r)
+		if err != nil {
+			return nil, err
+		}
+		ranges[i] = *rng
+	}
+
+	return &Selection{ranges}, nil
+}
+
+func unmarshalRange(data map[string]interface{}) (*Range, error) {
+	a, ok := parseNumber(data["anchor"])
+	if !ok {
+		return nil, ErrUnmarshalFailed
+	}
+	h, ok := parseNumber(data["head"])
+	if !ok {
+		return nil, ErrUnmarshalFailed
+	}
+	return &Range{a, h}, nil
+}
+
+func parseNumber(n interface{}) (int, bool) {
+	switch n.(type) {
+	case int:
+		return n.(int), true
+	case float64:
+		return int(n.(float64)), true
+	}
+	return 0, false
 }
--- ot/selection/selection_test.go
+++ ot/selection/selection_test.go
@@ -1,6 +1,7 @@
 package selection_test
 
 import (
+	"encoding/json"
 	"reflect"
 	"testing"
 
@@ -17,5 +18,85 @@ func TestSelectionTransform(t *testing.T) {
 		[]selection.Range{{8, 13}, {6, 15}},
 	}); !reflect.DeepEqual(actual, expected) {
 		t.Errorf("expected %+v, got %+v", expected, actual)
+	}
+}
+
+func TestMarshal(t *testing.T) {
+	for _, tc := range []struct {
+		input  *selection.Selection
+		output map[string]interface{}
+	}{
+		{input: &selection.Selection{}, output: map[string]interface{}{"ranges": []map[string]interface{}{}}},
+		{input: &selection.Selection{[]selection.Range{}}, output: map[string]interface{}{"ranges": []map[string]interface{}{}}},
+
+		{input: &selection.Selection{[]selection.Range{
+			{5, 8},
+		}}, output: map[string]interface{}{"ranges": []map[string]interface{}{
+			{"anchor": 5, "head": 8},
+		}}},
+
+		{input: &selection.Selection{[]selection.Range{
+			{2, 11},
+			{3, 7},
+		}}, output: map[string]interface{}{"ranges": []map[string]interface{}{
+			{"anchor": 2, "head": 11},
+			{"anchor": 3, "head": 7},
+		}}},
+	} {
+		actual := tc.input.Marshal()
+		if expected := tc.output; !reflect.DeepEqual(actual, expected) {
+			t.Errorf("expected marshaled %+v to equal %+v, got %+v", tc.input, expected, actual)
+		}
+	}
+}
+
+func TestUnmarshal(t *testing.T) {
+	for _, j := range []string{
+		`{"foo": "bar"}`,
+		`{"ranges": 123}`,
+		`{"ranges": "abc"}`,
+		`{"ranges": null}`,
+		`{"ranges": [{"anchor": 1, "tail": 2}]}`,
+		`{"ranges": [{"anchor": 1, "head": "shoulder"}]}`,
+	} {
+		var m map[string]interface{}
+		err := json.Unmarshal([]byte(j), &m)
+		if err != nil {
+			t.Fatalf("invalid test case")
+		}
+		_, err = selection.Unmarshal(m)
+		if err != selection.ErrUnmarshalFailed {
+			t.Errorf("expected ErrUnmarshalFailed when unmarshalling %+v, got %v", m, err)
+		}
+	}
+
+	for _, tc := range []struct {
+		input  string
+		output *selection.Selection
+	}{
+		{input: `{"ranges": []}`, output: &selection.Selection{[]selection.Range{}}},
+
+		{input: `{"ranges": [{"anchor": 1, "head": 3}]}`, output: &selection.Selection{[]selection.Range{
+			{1, 3},
+		}}},
+
+		{input: `{"ranges": [{"anchor": 2, "head": 5}, {"anchor": 1, "head": 4}]}`, output: &selection.Selection{[]selection.Range{
+			{2, 5},
+			{1, 4},
+		}}},
+	} {
+		var m map[string]interface{}
+		err := json.Unmarshal([]byte(tc.input), &m)
+		if err != nil {
+			t.Fatalf("invalid test case")
+		}
+
+		s, err := selection.Unmarshal(m)
+		if err != nil {
+			t.Errorf("expected no error, got +%v", err)
+		}
+		if actual, expected := s, tc.output; !reflect.DeepEqual(actual, expected) {
+			t.Errorf("expected %+v, got %+v", expected, actual)
+		}
 	}
 }
